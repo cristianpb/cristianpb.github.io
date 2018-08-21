df99eb83f5641b529daeb0bdc745f82501d033c8
cristianpb <felipebrokate@gmail.com>
2018-08-22 00:35:05 +0200

    Amp bulma (#1)
    * Add bulma.css
    Add gulpfile with purify
    
    * Include navbar and hero landing
    
    * Create columns for index.html
    
    * Use fontawesome icons
    Update blog link
    
    * Create package.json
    Update gulpfile and gitignore
    
    * Add pagination
    
    * Add replace for buma classes in markdown
    
    * Fix navbar always on top
    Fix regex to add class to bulma titles
    
    * Fix post content without replace
    Creation data from publications
    Include figures from publications
    
    * Add submodules bulma and bulma-timeline
    
    * Update exclude config
    Update about

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require('gulp'),
+  image = require('gulp-image'),
+  gp_uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var purify = require('gulp-purifycss');
+var minifyCss = require('gulp-minify-css');
+var replace = require('gulp-replace');
+ 
+gulp.task('templates', function(){
+  gulp.src(['./_posts_original/**.md'])
+    // See http://mdn.io/string.replace#Specifying_a_string_as_a_parameter
+    .pipe(replace(/### /gm, '{:.subtitle}\n###]]'))
+    .pipe(replace(/## /gm, '<br>\n\n{:.title}\n##]]'))
+    .pipe(replace(/{:.subtitle}\n###]]/gm, '{:.subtitle}\n### '))
+    .pipe(replace(/{:.title}\n##]]/gm, '{:.title}\n## '))
+    .pipe(gulp.dest('_posts/'));
+});
+
+gulp.task('image', function () {
+  gulp.src('./assets/images/*')
+    .pipe(image({
+      pngquant: true,
+      optipng: false,
+      zopflipng: true,
+      jpegRecompress: false,
+      mozjpeg: true,
+      guetzli: false,
+      gifsicle: true,
+      svgo: true,
+      concurrent: 10,
+      quiet: true // defaults to false
+    }))
+    .pipe(gulp.dest('./assets/images/'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./_sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./_includes/'));
+});
+
+// purify removes unused CSS classes
+gulp.task('purify', ['sass'],  function() {
+  return gulp.src('./_includes/main.css')
+    .pipe(purify(['_includes/**.html', './_layouts/**.html', './_pages/**.html', './blog/**.html'], {info: true}))
+    .pipe(replace(/!important/gm, ''))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./_includes/purify'));
+});
+
+gulp.task("watch", ["purify", "sass"], function() {
+  gulp.watch(["./_sass/main.scss", "./_sass/_variables.scss"], ["sass", "purify"])
+  gulp.watch(["./_pages/**", "./_layouts/**"], ["purify"]);
+})
+
+gulp.task('default', ['watch'], function(){});
