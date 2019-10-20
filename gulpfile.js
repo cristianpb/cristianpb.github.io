17efa4ded110779bc828f3c61d0b82b11985ff2a
cristianpb <felipebrokate@gmail.com>
2019-10-20 19:22:24 +0200

    Use imagemin to optimize images

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,15 @@
-var gulp = require('gulp'),
-  image = require('gulp-image');
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
 var sass = require('gulp-sass');
 var purify = require('gulp-purifycss');
-var replace = require('gulp-replace');
 let cleanCSS = require('gulp-clean-css');
 var responsive = require('gulp-responsive');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
- 
-gulp.task('replace', function(){
-  gulp.src(['./_posts/**.md'])
-    // See http://mdn.io/string.replace#Specifying_a_string_as_a_parameter
-    //.pipe(replace(/\/images\//gm, '/assets/img/'))
-    .pipe(replace(/figure:/gm, 'image:'))
-    .pipe(gulp.dest('_posts/'));
-});
 
 gulp.task('responsive', function () {
   return gulp.src('./assets/img/**/{main.png,main.jpg}')
     .pipe(responsive({
-      //'*/main.jpg': {
-      //  // Resize all JPG images to 200 pixels wide
-      //  width: 200,
-      //},
-      //'*/main.png': {
-      //  // Resize all PNG images to 50% of original pixels wide
-      //  width: '50%',
-      //},
       // Resize all images to 100 pixels wide and add suffix -thumbnail
       '*/main.{jpg,png}': {
         width: 300,
@@ -45,24 +28,34 @@ gulp.task('responsive', function () {
       withMetadata: false,
       crop: 'entropy'
     }))
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ], {verbose: true}))
     .pipe(gulp.dest('./assets/img'));
 });
 
 gulp.task('image', function () {
-  gulp.src('./assets/images/*')
-    .pipe(image({
-      pngquant: true,
-      optipng: false,
-      zopflipng: true,
-      jpegRecompress: false,
-      mozjpeg: true,
-      guetzli: false,
-      gifsicle: true,
-      svgo: true,
-      concurrent: 10,
-      quiet: true // defaults to false
-    }))
-    .pipe(gulp.dest('./assets/images/'));
+  gulp.src('assets/img/**/{*.png,*.jpg,*.jpeg,*.gif}')
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.svgo({
+        plugins: [
+          {removeViewBox: true},
+          {cleanupIDs: false}
+        ]
+      })
+    ], {verbose: true}))
+    .pipe(gulp.dest('assets/img'))
 });
 
 gulp.task('sass', function () {
@@ -80,7 +73,8 @@ gulp.task('purify', ['sass'],  function() {
 });
  
 gulp.task('concat', ['purify'], function() {
-  return gulp.src(['./assets/css/dist/main.css', './assets/css/syntax.css'])
+  //return gulp.src(['./assets/css/dist/main.css', './assets/css/syntax.css'])
+  return gulp.src(['./assets/css/dist/main.css'])
     .pipe(concat('main.css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({ suffix: '-min' }))
