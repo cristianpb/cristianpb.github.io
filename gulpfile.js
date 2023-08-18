3251b4075754037db14efe1d01c510c4855fb0d8
cristianpb <felipebrokate@gmail.com>
2023-08-18 14:29:27 +0200

    WIP add tailwind and daisyui

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var sass = require('gulp-sass');
 let cleanCSS = require('gulp-clean-css');
-var responsive = require('gulp-responsive');
+//var responsive = require('gulp-responsive');
 var purgecss = require('gulp-purgecss')
 var concat = require('gulp-concat');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
-var gulpAmpValidator = require('gulp-amphtml-validator');
+// var gulpAmpValidator = require('gulp-amphtml-validator');
 var del = require('del');
 
 var paths = {
@@ -31,100 +31,100 @@ var paths = {
   }
 };
 
-function thumbnails() {
-  return gulp.src([`${paths.images.src}/**/main.{png,jpg,jpeg,svg}`, `${paths.images.src}/external-articles/*.{png,jpg,jpeg,svg}`])
-    .pipe(
-      responsive({
-        '*': [
-          {
-            width: 400,
-            height: 225,
-            format: 'jpg',
-            rename: { 
-              suffix: '-thumb',
-              dirname: 'external-articles-responsive'
-            },
-          },
-          {
-            width: 400,
-            height: 225,
-            format: 'webp',
-            rename: { 
-              suffix: '-thumb',
-              dirname: 'external-articles-responsive'
-            },
-          },
-          {
-            width: 1400,
-            height: 788,
-            format: 'jpg',
-            rename: { 
-              suffix: '-16x9',
-              dirname: 'external-articles-responsive'
-            },
-          },
-          {
-            width: 1400,
-            height: 788,
-            format: 'webp',
-            rename: { 
-              suffix: '-16x9',
-              dirname: 'external-articles-responsive'
-            },
-          }
-        ],
-        '*/main.*': [
-          {
-            width: 400,
-            height: 225,
-            format: 'jpg',
-            rename: { suffix: '-thumb'},
-          },
-          {
-            width: 400,
-            height: 225,
-            format: 'webp',
-            rename: { suffix: '-thumb'},
-          },
-          {
-            width: 1400,
-            height: 788,
-            format: 'jpg',
-            rename: { suffix: '-16x9'},
-          },
-          {
-            width: 1400,
-            height: 788,
-            format: 'webp',
-            rename: { suffix: '-16x9'},
-          }
-        ],
-      }, {
-        // Global configuration for all images
-        // The output quality for JPEG, WebP and TIFF output formats
-        quality: 70,
-        // Use progressive (interlace) scan for JPEG and PNG output
-        progressive: true,
-        // Zlib compression level of PNG output format
-        compressionLevel: 6,
-        // Strip all metadata
-        withMetadata: false,
-        errorOnEnlargement: false,
-        crop: 'entropy'
-      }))
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
-        ]
-      })
-    ], {verbose: true}))
-    .pipe(gulp.dest(paths.images.dest));
-}
+// function thumbnails() {
+//   return gulp.src([`${paths.images.src}/**/main.{png,jpg,jpeg,svg}`, `${paths.images.src}/external-articles/*.{png,jpg,jpeg,svg}`])
+//     .pipe(
+//       responsive({
+//         '*': [
+//           {
+//             width: 400,
+//             height: 225,
+//             format: 'jpg',
+//             rename: { 
+//               suffix: '-thumb',
+//               dirname: 'external-articles-responsive'
+//             },
+//           },
+//           {
+//             width: 400,
+//             height: 225,
+//             format: 'webp',
+//             rename: { 
+//               suffix: '-thumb',
+//               dirname: 'external-articles-responsive'
+//             },
+//           },
+//           {
+//             width: 1400,
+//             height: 788,
+//             format: 'jpg',
+//             rename: { 
+//               suffix: '-16x9',
+//               dirname: 'external-articles-responsive'
+//             },
+//           },
+//           {
+//             width: 1400,
+//             height: 788,
+//             format: 'webp',
+//             rename: { 
+//               suffix: '-16x9',
+//               dirname: 'external-articles-responsive'
+//             },
+//           }
+//         ],
+//         '*/main.*': [
+//           {
+//             width: 400,
+//             height: 225,
+//             format: 'jpg',
+//             rename: { suffix: '-thumb'},
+//           },
+//           {
+//             width: 400,
+//             height: 225,
+//             format: 'webp',
+//             rename: { suffix: '-thumb'},
+//           },
+//           {
+//             width: 1400,
+//             height: 788,
+//             format: 'jpg',
+//             rename: { suffix: '-16x9'},
+//           },
+//           {
+//             width: 1400,
+//             height: 788,
+//             format: 'webp',
+//             rename: { suffix: '-16x9'},
+//           }
+//         ],
+//       }, {
+//         // Global configuration for all images
+//         // The output quality for JPEG, WebP and TIFF output formats
+//         quality: 70,
+//         // Use progressive (interlace) scan for JPEG and PNG output
+//         progressive: true,
+//         // Zlib compression level of PNG output format
+//         compressionLevel: 6,
+//         // Strip all metadata
+//         withMetadata: false,
+//         errorOnEnlargement: false,
+//         crop: 'entropy'
+//       }))
+//     .pipe(imagemin([
+//       imagemin.gifsicle({interlaced: true}),
+//       imagemin.jpegtran({progressive: true}),
+//       imagemin.optipng({optimizationLevel: 5}),
+//       imagemin.svgo({
+//         plugins: [
+//           {removeViewBox: true},
+//           {cleanupIDs: false}
+//         ]
+//       })
+//     ], {verbose: true}))
+//     .pipe(gulp.dest(paths.images.dest));
+// }
 
 
 function images() {
@@ -143,12 +143,12 @@ function images() {
     .pipe(gulp.dest(paths.images.dest))
 }
 
-function amp_validator() {
-  return gulp.src(paths.html.src)
-    .pipe(gulpAmpValidator.validate())
-    .pipe(gulpAmpValidator.format())
-    .pipe(gulpAmpValidator.failAfterError());
-}
+//function amp_validator() {
+//  return gulp.src(paths.html.src)
+//    .pipe(gulpAmpValidator.validate())
+//    .pipe(gulpAmpValidator.format())
+//    .pipe(gulpAmpValidator.failAfterError());
+//}
  
 function clean() {
   return del([ paths.styles.tmp ]);
